803497119b106fb0f6d7b8e2f33a8052d5c8dbf3
Greig <gstewart@streamba.net>
2015-05-08 15:11:21 +0100

    copy areas within the texture

--- Combined2DAnd3D/Program.cs
+++ Combined2DAnd3D/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Threading;
 using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 using Combined2DAnd3D.ImageCompare;
 using SharpDX.DXGI;
 
@@ -22,7 +23,7 @@ namespace Combined2DAnd3D
             var factory1 = new Factory1();
             var adapter1 = factory1.GetAdapter1(0);
             var device10 = new SharpDX.Direct3D10.Device1(adapter1);
-            var ptrVal = ((long)1073754690); // handle of shared texture
+            var ptrVal = ((long)1073761026); // handle of shared texture
 
             var textureD3D10 = device10.OpenSharedResource<SharpDX.Direct3D10.Texture2D>(new IntPtr(ptrVal));
 
@@ -30,40 +31,50 @@ namespace Combined2DAnd3D
             {
                 try
                 {
+                    //Console.ReadLine();
 
-                        string filename = "c:\\temp\\screens\\" + DateTime.Now.Ticks + ".png";
-                        var stream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite);
-                        var bitmap = textureD3D10.CopyToBitmap(stream, device10);
-                        bitmap.Save(stream,ImageFormat.Bmp);                        
-                        stream.Flush();
-                        stream.Close();
-                        stream.Dispose();
-
-                        Thread.Sleep(5000);
-
-                        filename = "c:\\temp\\screens\\" + DateTime.Now.Ticks + ".png";
-                        stream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite);
-                        var bitmap2 = textureD3D10.CopyToBitmap(stream, device10);
-                        bitmap2.Save(stream, ImageFormat.Bmp);
-                        stream.Flush();
-                        stream.Close();
-                        stream.Dispose();
+                    var areas = new[]
+                    {
+                        new Rectangle(100,100,100,100),
+                        new Rectangle(200,200,100,100)
+                    };
 
 
+                    var bitmaps = textureD3D10.SplitIntoBitmapSegments(device10, areas);
+                    SaveToDisk(bitmaps[0]);
+                    SaveToDisk(bitmaps[1]);
+                    
+                    //Console.ReadLine();
 
-                        TestComparison(bitmap, bitmap2);
+                    //var bitmap2 = textureD3D10.CopyToBitmap(device10);
+                    //SaveToDisk(bitmap2);
+
+                    //TestComparison(bitmap, bitmap2);
+
+                    Thread.Sleep(1000);
+
                 }
-                catch
+                catch (Exception ex)
                 {
-
                 }
-
             }
+        }
 
+
+        private static void SaveToDisk(Bitmap bitmap)
+        {
+            string filename = "c:\\temp\\screens\\" + DateTime.Now.Ticks + ".png";
+            var stream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite);
+            bitmap.Save(stream, ImageFormat.Bmp);
+            stream.Flush();
+            stream.Close();
+            stream.Dispose();
         }
 
         private static void TestComparison(Bitmap bitmap, Bitmap bitmap2)
         {
+            // The threshold is the minimal acceptable similarity between template candidate. 
+            // Min (loose) is 0.0 Max (strict) is 1.0
             const float similarityThreshold = 0.50f;
 
 
@@ -82,7 +93,7 @@ namespace Combined2DAnd3D
             }
 
             Console.WriteLine("End of comparison.");
-            Console.ReadLine();
+
         }
     }
 }
--- Combined2DAnd3D/Texture2DExtensions.cs
+++ Combined2DAnd3D/Texture2DExtensions.cs
@@ -1,16 +1,87 @@
 ﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using SharpDX;
 using SharpDX.Direct3D10;
 using SharpDX.DXGI;
+using Rectangle = System.Drawing.Rectangle;
 
 namespace Combined2DAnd3D
 {
     public static class Texture2DExtensions
     {
 
-        public static System.Drawing.Bitmap CopyToBitmap(this Texture2D texture, Stream stream, SharpDX.Direct3D10.Device device)
+        public static Bitmap[] SplitIntoBitmapSegments(this Texture2D texture, SharpDX.Direct3D10.Device device, System.Drawing.Rectangle[] areas)
+        {
+            var textureCopy = new Texture2D(device, new Texture2DDescription
+            {
+                Width = texture.Description.Width,
+                Height = texture.Description.Height,
+                MipLevels = 1,
+                ArraySize = 1,
+                Format = texture.Description.Format,
+                Usage = ResourceUsage.Staging,
+                SampleDescription = new SampleDescription(1, 0),
+                BindFlags = BindFlags.None,
+                CpuAccessFlags = CpuAccessFlags.Read,
+                OptionFlags = ResourceOptionFlags.None
+            });
+            device.CopyResource(texture, textureCopy);
+
+            DataStream mipsize;
+            var dataBox = textureCopy.Map(
+                0,
+                MapMode.Read,
+                SharpDX.Direct3D10.MapFlags.None,
+                out mipsize);
+
+            var bitmaps = new Bitmap[areas.Length];
+
+
+            for(var i = 0; i < areas.Length; i++)
+            {
+                var area = areas[i];
+                var bitmap = new Bitmap(area.Width, area.Height, PixelFormat.Format32bppArgb);
+                var boundsRect = new Rectangle(0,0,area.Width,area.Height);
+
+
+                // Copy pixels from screen capture Texture to GDI bitmap
+                var mapDest = bitmap.LockBits(boundsRect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                var sourcePtr = dataBox.DataPointer;
+
+                var destPtr = mapDest.Scan0;
+                
+                // set start position
+                // get to the right row
+                sourcePtr = IntPtr.Add(sourcePtr, dataBox.Pitch * area.Top);
+
+                for (int y = 0; y < area.Height; y++)
+                {
+                    // Move to the start position within this row
+                    sourcePtr = IntPtr.Add(sourcePtr, 4 * (area.Left));
+                    // Copy the relevant pixels in this row
+                    Utilities.CopyMemory(destPtr, sourcePtr, 4 * area.Width);
+
+                    // Advance pointer to the end of the copied data
+                    sourcePtr = IntPtr.Add(sourcePtr, area.Width * 4);
+
+                    // Advance pointers to next row
+                    sourcePtr = IntPtr.Add(sourcePtr, dataBox.Pitch - (4 * (area.Right)));
+                    destPtr = IntPtr.Add(destPtr, mapDest.Stride);
+                }
+
+                bitmap.UnlockBits(mapDest);
+
+                bitmaps[i] = bitmap;
+                
+            }
+
+            return bitmaps;
+
+        }
+
+
+        public static System.Drawing.Bitmap CopyToBitmap(this Texture2D texture, SharpDX.Direct3D10.Device device)
         {
             var textureCopy = new Texture2D(device, new Texture2DDescription
             {
