8b3ea001985e98d22a24f130cc776689dde452ed
Greig <gstewart@streamba.net>
2015-11-25 17:08:06 +0000

    sending icons over socket

--- Combined2DAnd3D/Program.cs
+++ Combined2DAnd3D/Program.cs
@@ -2,124 +2,165 @@
 using System.Drawing.Imaging;
 using System.IO;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Combined2DAnd3D.ImageCompare;
+using SharpDX.Direct3D10;
 using SharpDX.DXGI;
+using Device1 = SharpDX.Direct3D10.Device1;
 
 namespace Combined2DAnd3D
 {
 
-    internal static class Program
+    public class Program : IMessageComputer
     {
 
         public static void Main()
         {
-            Run();
+            new Program().Start();
         }
 
-        public static void Run()
+        private Texture2D textureD3D10;
+        private IEnumerable<Bitmap> icons;
+        private Device1 device10;
+        private string[] filenames = new string[]
+        {
+                        "sword",
+                        "dirt",
+                        "pickaxe",
+                        "sand",
+                        "steak",
+                        "sword",
+                        "torch",
+                        "wood",
+                        "iron",
+                        "stone",
+                        "sword_dark",
+                        "dirt_dark",
+                        "pickaxe_dark",
+                        "sand_dark",
+                        "steak_dark",
+                        "sword_dark",
+                        "torch_dark",
+                        "wood_dark",
+                        "iron_dark",
+                        "stone_dark",
+                        "empty"
+
+        };
+
+        public void Start()
+        {
+            Run();
+            new SynchronousSocketListener(this).Start();
+            
+        }
+
+        public void Run()
         {
             var factory1 = new Factory1();
             var adapter1 = factory1.GetAdapter1(0);
-            var device10 = new SharpDX.Direct3D10.Device1(adapter1);
+            
+            device10 = new SharpDX.Direct3D10.Device1(adapter1);
             var ptrVal = ((long)-2147476734); // handle of shared texture
 
-            var filenames = new string[]
+
+
+
+            
+            icons = filenames.Select(LoadBmp);
+
+
+            textureD3D10 = device10.OpenSharedResource<SharpDX.Direct3D10.Texture2D>(new IntPtr(ptrVal));
+        }
+
+        private string ComputeSingleIconSet()
+        {
+            string result = string.Empty;
+            try
             {
-                "sword",
-                "dirt",
-                "pickaxe",
-                "sand",
-                "steak",
-                "sword",
-                "torch",
-                "wood",
-                "iron",
-                "stone",
-                "empty"
-            };
+
+                const int distancebetween = 80;
+                const int start = 1920 - 1308;
+                var areas = new[]
+                {
+                        new Rectangle(start + distancebetween * 0, 13,56,56),
+                        new Rectangle(start + distancebetween * 1 ,13,56,56),
+                        new Rectangle(start + distancebetween * 2 ,13,56,56),
+                        new Rectangle(start + distancebetween * 3 ,13,56,56),
+                        new Rectangle(start + distancebetween * 4 ,13,56,56),
+                        new Rectangle(start + distancebetween * 5 ,13,56,56),
+                        new Rectangle(start + distancebetween * 6 ,13,56,56),
+                        new Rectangle(start + distancebetween * 7 ,13,56,56),
+                        new Rectangle(start + distancebetween * 8 ,13,56,56),
+                    };
 
 
-            var icons = filenames.Select(LoadBmp);
+                var bitmaps = textureD3D10.SplitIntoBitmapSegments(device10, areas);
 
-            var textureD3D10 = device10.OpenSharedResource<SharpDX.Direct3D10.Texture2D>(new IntPtr(ptrVal));
+                //Console.ReadLine();
 
-             while (true)
-             {
-                 try
-                 {
-                 
-                    const int distancebetween = 80;
-                     const int start = 1920 - 1308;
-                     var areas = new[]
-                     {
-                         new Rectangle(start + distancebetween * 0, 13,56,56),
-                         new Rectangle(start + distancebetween * 1 ,13,56,56),
-                         new Rectangle(start + distancebetween * 2 ,13,56,56),
-                         new Rectangle(start + distancebetween * 3 ,13,56,56),
-                         new Rectangle(start + distancebetween * 4 ,13,56,56),
-                         new Rectangle(start + distancebetween * 5 ,13,56,56),
-                         new Rectangle(start + distancebetween * 6 ,13,56,56),
-                         new Rectangle(start + distancebetween * 7 ,13,56,56),
-                         new Rectangle(start + distancebetween * 8 ,13,56,56),
-                     };
+                //var bitmap2 = textureD3D10.CopyToBitmap(device10);
+                
+                var position = 0;
 
+                foreach (var bitmap in bitmaps)
+                {
+                    position++;
+                    var changed = ChangePixelFormat(bitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
-                     var bitmaps = textureD3D10.SplitIntoBitmapSegments(device10, areas);
+                    SaveToDisk(changed);
 
-                    //Console.ReadLine();
+                    var highest = 0f;
+                    string highesticonname = null;
 
-                    //var bitmap2 = textureD3D10.CopyToBitmap(device10);
-                    //SaveToDisk(bitmap2);
-                     var position = 0;
-                     foreach (var bitmap in bitmaps)
-                     {
-                        position++;
-                        var changed = ChangePixelFormat(bitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-
-                        //SaveToDisk(changed);
-
-                        var highest = 0f;
-                        string highesticonname = null;
-
-                        var count = 0;
-                        foreach (var icon in icons)
+                    var count = 0;
+                    foreach (var icon in icons)
+                    {
+                        var tmp = TestComparison(changed, icon);
+                        if (tmp > highest)
                         {
-                            var tmp = TestComparison(changed, icon);
-                            if (tmp > highest)
-                            {
-                                highest = tmp;
-                                highesticonname = filenames[count];
-                            }
-                            count++;
+                            highest = tmp;
+                            highesticonname = filenames[count];
                         }
-
-                         if (highest > 0.78f)
-                         {
-                            Console.WriteLine(position + ": " + highest + " " + highesticonname);
-                         }
-                         else
-                         {
-                            Console.WriteLine(position + ": " + highest + " empty");
-                        }
-          
-                        
+                        count++;
                     }
 
+                    string message;
+                   
+
+                    if (highest > 0.78f)
+                    {
+                        message = position + ":" + highesticonname;
+                    }
+                    else
+                    {
+                        message = position + ":empty";
+                    }
+
+                    Console.WriteLine(message);
+                    if (result == string.Empty)
+                    {
+                        result += message;
+                    }
+                    else
+                    {
+                        result += "," + message;
+                    }
+
+                }
 
 
-                     Thread.Sleep(3000);
-                     Console.WriteLine();
 
+            }
+            catch (Exception ex)
+            {
+            }
 
+            return result;
 
-                 }
-                 catch (Exception ex)
-                 {
-                 }
-             }
-        }
+        } 
 
         private static Bitmap LoadBmp(string filename)
         {
@@ -158,6 +199,11 @@ namespace Combined2DAnd3D
 
             return test;
 
+        }
+
+        public string GetMessage()
+        {
+            return ComputeSingleIconSet();
         }
     }
 }
---
+++ Server/SynchronousSocketListener.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public interface IMessageComputer
+{
+    string GetMessage();
+}
+
+public class MessageComputer : IMessageComputer
+{
+    public string GetMessage()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+public class SynchronousSocketListener
+{
+    private readonly IMessageComputer _messageComputer;
+    // Incoming data from the client.
+    public static string data = null;
+    public static Stack<string> stack = new Stack<string>();
+
+
+    public SynchronousSocketListener(IMessageComputer messageComputer)
+    {
+        _messageComputer = messageComputer;
+    }
+
+
+
+    public void StartListening()
+    {
+        // Data buffer for incoming data.
+        byte[] bytes = new Byte[1024];
+
+        // Establish the local endpoint for the socket.
+        // Dns.GetHostName returns the name of the 
+        // host running the application.
+        IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[1];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 1024);
+
+        // Create a TCP/IP socket.
+        Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and 
+        // listen for incoming connections.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            // Start listening for connections.
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+                // Program is suspended while waiting for an incoming connection.
+                Socket handler = listener.Accept();
+
+
+
+                // An incoming connection needs to be processed.
+                while (true)
+                {
+                    try
+                    {
+                        bool found = false;
+                        while (!found)
+                        {
+                            bytes = new byte[1024];
+                            int bytesRec = handler.Receive(bytes);
+                            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                            if (data.IndexOf("|") > -1)
+                            {
+                                found = true;
+                            }
+                        }
+
+                        Console.WriteLine("Text received : {0}", data);
+
+
+                        while (stack.Count == 0)
+                        {
+                            Thread.Sleep(10);
+                        }
+                        var poppedMsg = stack.Pop();
+                        var response = _messageComputer.GetMessage();
+                        byte[] msg = Encoding.ASCII.GetBytes(response);
+
+                        handler.Send(msg);
+
+                    }
+                    catch (SocketException socketException)
+                    {
+                        Console.WriteLine("Client closed the socket");
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                        handler = listener.Accept();
+                    }
+
+
+                    data = null;
+
+                }
+ 
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+                // Show the data on the console.
+
+
+   
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+
+    public void Start()
+    {
+        var threadStart = new ThreadStart(StartListening);
+        var t = new Thread(threadStart);
+        t.Start();
+
+        var dumpdata = new ThreadStart(DumpData);
+        var tdump = new Thread(dumpdata);
+        tdump.Start();
+        t.Join();
+    }
+
+    private static void DumpData()
+    {
+        while (true)
+        {
+            Thread.Sleep(10);
+            stack.Push(DateTime.Now.Ticks.ToString());
+        }
+
+    }
+}
